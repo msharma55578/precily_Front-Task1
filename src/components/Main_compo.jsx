58d6ddefbc66158027e3c96e979563ca02aa9b09
msharma55578 <m.sharma55578@gmail.com>
2020-11-04 03:36:57 +0530

    pre final

--- src/components/Main_compo.jsx
+++ src/components/Main_compo.jsx
@@ -4,17 +4,62 @@ import axios from 'axios';
 export default function Main_compo() {
     const [name,setname]= useState('');
     const [age,setage]= useState('');
+    const [id,setid]= useState('');
 
     const submitAdd =()=>{
-        axios.post("http://localhost:8080/add",{
+        if(name===''&&age===''){
+            alert("Atleast 1 field Required")
+        }else{
+            axios.post("http://localhost:8080/add",{
             name:name,
             age:age,
-        }).then(()=>{
-            console.log("success insert")
         })
+        .then( ()=>{
+            alert("successfully inserted")     ///alert.......
+            console.log("insert")
+            setname('');
+            setage('');
+        })
+        }
+    }
+
+    const submitUpdate =()=>{
+        if(id==='')  alert("id required for update")
+        else{
+            axios.put("http://localhost:8080/update",{
+            name:name,
+            age:age,
+            id:id
+            }).then(()=>{
+                alert("successfully updated")     ///alert.......
+                setid('');
+                setname('');
+                setage('');
+            })
+        }
+    }
+    const countApi =()=>{
+        axios.get("http://localhost:8080/count",{
+        }).then((response)=>{
+            console.log(response.data)
+            let add_cnt=response.data[0].count;
+            let update_cnt=response.data[1].count
+            alert("Total Add hit "+add_cnt+" and Upadte API hit "+update_cnt)
+        })
+    
     }
     return (
         <div>
+            <h2>Enter the value for add and update Field</h2>
+            <input
+                    type="text"
+                    placeholder="Enter Id for Update"
+                    // className="question-container"
+                    value={id}
+                    onChange={(evt) => {
+                    setid(evt.target.value);
+                }}
+            />
             <input
                 type="text"
                 placeholder="Enter Name"
@@ -34,10 +79,9 @@ export default function Main_compo() {
             }}
             />
             <button onClick={submitAdd}>ADD</button>
-
-            <div><button>UPDATE</button></div>
-            
-            <div><button>COUNT</button></div>
+            <button onClick={submitUpdate}>UPDATE</button>
+            <h2>Total Api Count</h2>            
+            <div><button onClick={countApi}>COUNT</button></div>
         </div>
     )
 }
